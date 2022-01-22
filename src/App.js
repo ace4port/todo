9c264702587e534faf96562b1bd5c680dedfb418
ace4port <39233007+ace4port@users.noreply.github.com>
2022-01-22 11:33:18 +0545

    Init with updated dependencies

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react'
 import TodoList from './TodoList'
-import uuidv4 from 'uuid/v4'
+import { v4 as uuidv4 } from 'uuid'
 
 const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
-  const [todos, setTodos] = useState([])
   const todoNameRef = useRef()
+  const [todos, setTodos] = useState([])
 
   useEffect(() => {
     const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
@@ -19,7 +19,7 @@ function App() {
 
   function toggleTodo(id) {
     const newTodos = [...todos]
-    const todo = newTodos.find(todo => todo.id === id)
+    const todo = newTodos.find((todo) => todo.id === id)
     todo.complete = !todo.complete
     setTodos(newTodos)
   }
@@ -27,26 +27,34 @@ function App() {
   function handleAddTodo(e) {
     const name = todoNameRef.current.value
     if (name === '') return
-    setTodos(prevTodos => {
-      return [...prevTodos, { id: uuidv4(), name: name, complete: false}]
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: uuidv4(), name: name, complete: false }]
     })
     todoNameRef.current.value = null
   }
 
   function handleClearTodos() {
-    const newTodos = todos.filter(todo => !todo.complete)
+    const newTodos = todos.filter((todo) => !todo.complete)
     setTodos(newTodos)
   }
 
   return (
-    <div className='container'>
-      <TodoList todos={todos} toggleTodo={toggleTodo} />
-      <input ref={todoNameRef} type="text" /><br />
-      <button onClick={handleAddTodo} className='btn' >Add Todo</button>
-      <button onClick={handleClearTodos} className='btn'>Clear Complete</button>
-      <div className='item'>{todos.filter(todo => !todo.complete).length} left to do</div>
-    </div>
+    <>
+      <h3 class='item'>Add items, remove and clear</h3>
+      <div className='container'>
+        <TodoList todos={todos} toggleTodo={toggleTodo} />
+        <input ref={todoNameRef} type='text' />
+        <br />
+        <button onClick={handleAddTodo} className='btn'>
+          Add Todo
+        </button>
+        <button onClick={handleClearTodos} className='btn'>
+          Clear Complete
+        </button>
+        <div className='item'>{todos.filter((todo) => !todo.complete).length} left to do</div>
+      </div>
+    </>
   )
 }
 
-export default App;
+export default App
